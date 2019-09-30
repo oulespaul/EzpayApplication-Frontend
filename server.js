c5ae3a4e0ae6504248f155f98bce6edd00ccff16
Thanakrit  Choutkhajung <oulespaul@gmail.com>
2019-09-30 15:43:11 +0700

    deploy2

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-let app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+const port = process.env.PORT || 8080
+app.listen(port)
